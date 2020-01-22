4a5a7b4933c6b793c271e909ebae774a9c28e135
Shehzad Abdulcadir <shehzadac130@gmail.com>
2020-01-22 20:18:55 +0000

    [Styles] - Add SCSS and base styles

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+// Nav
+const mainNav = document.getElementById('js-menu');
+const navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
+});
