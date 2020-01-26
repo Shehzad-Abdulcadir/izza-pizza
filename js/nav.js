a8dd84a13359ec53b86a70b8a535438317822ece
Shehzad Abdulcadir <shehzadac130@gmail.com>
2020-01-26 21:41:12 +0000

    [Header] - Add JS for sticky header

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,25 @@
-// Nav
+// Mobile Nav
 const mainNav = document.getElementById('js-menu');
 const navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function() {
   mainNav.classList.toggle('d-block');
+});
+
+// Sticky on scroll
+let scrollpos = window.scrollY;
+const header = document.getElementById('js-header');
+const headerHeight = header.offsetHeight;
+
+const addClassOnScroll = () => header.classList.add('sticky');
+const removeClassOnScroll = () => header.classList.remove('sticky');
+
+window.addEventListener('scroll', function() {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= headerHeight) {
+    addClassOnScroll();
+  } else {
+    removeClassOnScroll();
+  }
 });
